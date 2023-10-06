7af9423b9727cc9a34706e3d76ed2ea41c30f364
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-10-06 20:39:40 -0300

    add: registration form validations

--- front/src/app/signup/page.jsx
+++ front/src/app/signup/page.jsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@/components/RegisterForm";
 
 const SignupPage = () => {
   return (
-    <main>
+    <main className="flex justify-center items-center min-h-screen p-8">
       <RegisterForm />
     </main>
   );
