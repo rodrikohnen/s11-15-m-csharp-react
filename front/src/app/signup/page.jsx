83434c3b29220815b34b49893063b0f573cd15c2
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-10-06 15:51:25 -0300

    add: Componets Folder and SignUp Page

---
+++ front/src/app/signup/page.jsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components/RegisterForm";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default SignupPage;
