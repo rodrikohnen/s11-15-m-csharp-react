8d405447b11914a960c2a55328a7446ed5a3541e
matias torres <matias.torres1812@gmail.com>
2023-10-09 19:40:22 -0300

    changes: login

--- front/src/app/login/page.jsx
+++ front/src/app/login/page.jsx
@@ -3,13 +3,8 @@ import Link from "next/link";
 
 export default function Login() {
   return (
-<<<<<<< HEAD
     <main className="flex-col">
       <div className="text-3xl flex justify-start flex-col mt-12 mb-16 ml-4">
-=======
-    <main className="text-center">
-      <div className="text-3xl p-[2rem]">
->>>>>>> 21074f5e287ebf653370e6c008584f1abe85c68b
         <h1>Iniciar sesión</h1>
         <Link className="text-sm text-sky-500" href="/signup">
           ¿Aun no tienes cuenta?
