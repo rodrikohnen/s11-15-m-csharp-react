1bd62d18b17c27a7801b235529c832c201c4bb96
matias torres <matias.torres1812@gmail.com>
2023-10-09 14:43:00 -0300

    changes: button replaced a in navbar and finish the login

--- front/src/app/login/page.jsx
+++ front/src/app/login/page.jsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <main className="flex-col">
       <NavBar/>
-      <div className='text-3xl flex justify-start flex-col mt-8 mb-12 ml-4'>
+      <div className='text-3xl flex justify-start flex-col mt-12 mb-24 ml-4'>
           <h1>Iniciar sesión</h1>
           <a className="text-sm text-sky-500">
             ¿Aun no tienes cuenta?
--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -4,12 +4,12 @@ export default function NavBar() {
       className="flex flex-row mt-4 justify-around border-black  border-b-[1.5px] pb-3 w-[24.5rem]">
       <h2 className=" p-2">LOGO</h2>
       <div className="flex flex-row">
-        <button className="rounded-full px-4 py-2 text-sm	 min-w-min sm:text-base">
+        <a className="rounded-full px-4 py-2 text-sm	 min-w-min sm:text-base" href="/signup">
           Registrarme
-        </button>
-        <button className="rounded-full text-white	text-sm	 px-4 py-2 bg-gray-600	 min-w-fit sm:text-base">
+        </a>
+        <a className="rounded-full text-white	text-sm	 px-4 py-2 bg-gray-600 min-w-fit sm:text-base " href="/login">
           Iniciar Sesion
-        </button>
+        </a>
       </div>
     </div>
   );
