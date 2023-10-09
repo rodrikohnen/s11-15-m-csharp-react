120fd1810f88cc45878eeaa73ae97bee9cca78ce
montoyitadevelp <simon1018226487@gmail.com>
2023-10-09 14:31:37 -0500

    add: Svg components

---
+++ front/src/components/svg/Svgs.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const CircleHome = () => {
+  return (
+    <>
+      <div className="rounded-full bg-danger-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="300"
+          height="300"
+          viewBox="0 0 160 160"
+          fill="none"
+        >
+          <circle cx="80" cy="80" r="80" fill="#D9D9D9" />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+export const CircleChoices = () => {
+  return (
+    <>
+      <div className="rounded-full bg-danger-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 160 160"
+          fill="none"
+        >
+          <circle cx="80" cy="80" r="80" fill="#D9D9D9" />
+        </svg>
+      </div>
+    </>
+  );
+};
+
