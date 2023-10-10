ef23a20dcb47fc939dd36598655246e6919834ca
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-10-10 10:35:30 -0300

    Change: Card Test

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -1,29 +1,32 @@
-import CardEnVivo from "@/components/CardEnVivo";
-import Link from "next/link";
+/* import CardEnVivo from "@/components/CardEnVIvo";
+ */ import Link from "next/link";
 
 export default function HomeUser() {
-
   const cardsData = [1, 2, 3, 4];
 
   return (
     <main className="flex-col">
       <div className="text-3xl flex justify-start flex-col mt-12 mb-24 ml-4">
         <h1>¡Hola!</h1>
-      <span className="flex flex-col justify-center items-center text-center">
-      <Link href="" className=" bg-gray-400 m-2 rounded-full text-white text-center	text-base	w-[15rem] h-[2.5rem]">
-        Quiero ser tutor
-      </Link>
-      <Link href="" className=" bg-gray-400 m-2 rounded-full text-white text-center	text-base	w-[15rem] h-[2.5rem]">
-        Quiero ser mentor
-      </Link>
-      </span>
+        <span className="flex flex-col justify-center items-center text-center">
+          <Link
+            href=""
+            className=" bg-gray-400 m-2 rounded-full text-white text-center	text-base	w-[15rem] h-[2.5rem]">
+            Quiero ser tutor
+          </Link>
+          <Link
+            href=""
+            className=" bg-gray-400 m-2 rounded-full text-white text-center	text-base	w-[15rem] h-[2.5rem]">
+            Quiero ser mentor
+          </Link>
+        </span>
         <h1>En vivo</h1>
-        <span className="flex flex-col justify-start items-start ">
+        {/*         <span className="flex flex-col justify-start items-start ">
           {cardsData.map((item, index) => (
             <CardEnVivo key={index} />
           ))}
-        </span>
-      </div> 
+        </span> */}
+      </div>
     </main>
   );
 }
