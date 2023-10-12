a8a67e1736472a8faca4367d1095a3152fb9f229
montoyitadevelp <simon1018226487@gmail.com>
2023-10-12 14:16:31 -0500

    fix: Remove margin in login page

--- front/src/app/login/page.jsx
+++ front/src/app/login/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Login() {
   return (
     <main className="flex-col text-center">
-      <div className="text-3xl flex justify-start flex-col mt-12 mb-16 ml-4">
+      <div className="text-3xl flex justify-start flex-col mt-12 mb-16 ">
         <h1>Iniciar sesión</h1>
         <Link className="text-sm text-sky-500" href="/signup">
           ¿Aun no tienes cuenta?
