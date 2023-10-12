9dfe9a940e0a9d695496d18923a8283ce1721370
montoyitadevelp <simon1018226487@gmail.com>
2023-10-12 10:39:13 -0500

    fix: Improve justify in login

---
+++ front/src/app/login/page.jsx
@@ -0,0 +1,16 @@
+import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <main className="flex-col text-center">
+      <div className="text-3xl flex justify-start flex-col mt-12 mb-16 ml-4">
+        <h1>Iniciar sesión</h1>
+        <Link className="text-sm text-sky-500" href="/signup">
+          ¿Aun no tienes cuenta?
+        </Link>
+      </div>
+      <LoginForm />
+    </main>
+  );
+}
