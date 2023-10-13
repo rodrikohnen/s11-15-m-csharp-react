eeb6173ee4978c00a94a49f5b9ae4c68816b1db1
matias torres <matias.torres1812@gmail.com>
2023-10-13 18:25:00 -0300

    add: buenos dias

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -1,24 +1,32 @@
 import CardEnVivo from "@/components/CardEnVIvo";
+import CardMisGrupos from "@/components/CardMisGrupos";
 import Link from "next/link";
 
 export default function HomeUser() {
   const cardsData = [1, 2, 3, 4];
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "";
+
+  if (currentHour >= 5 && currentHour < 12) {
+    greeting = "Buenos días 🌤️";
+  } else if (currentHour >= 12 && currentHour < 22) {
+    greeting = "Buenas tardes 🌄";
+  } else {
+    greeting = "Buenas noches 🌙";
+  }
 
   return (
     <main className="flex-col">
-      <div className="text-3xl flex justify-start flex-col mt-12 mb-24 ml-4">
-        <h1>¡Hola!</h1>
-        <span className="flex flex-col justify-center items-center text-center">
-          <Link
-            href=""
-            className=" bg-gray-400 m-2 rounded-full text-white text-center	text-base	w-[15rem] h-[2.5rem]">
-            Quiero ser tutor
-          </Link>
-          <Link
-            href=""
-            className=" bg-gray-400 m-2 rounded-full text-white text-center	text-base	w-[15rem] h-[2.5rem]">
-            Quiero ser mentor
-          </Link>
+      <div className="text-3xl flex justify-start flex-col mt-12  ml-4">
+        <h1 className="mb-4">{greeting}</h1>
+        <span className="flex flex-row justify-start items-start text-sm">
+          <input type="text" placeholder="Search" className="border border-zinc-400 mb-8 w-[17rem] h-[2rem] rounded-sm" />
+        </span>
+        <h1>Mis grupos</h1>
+        <span className="flex flex-col justify-start items-start ">
+          <CardMisGrupos/>
         </span>
         <h1>En vivo</h1>
         <span className="flex flex-col justify-start items-start ">
