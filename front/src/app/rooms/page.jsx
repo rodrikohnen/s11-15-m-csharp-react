6fbab3f10cd9cfadb0bae2f70e47ddf8749d1ab3
sofiacostamagna <sofiacostamagna45@gmail.com>
2023-10-19 09:27:05 -0300

    add: changes rooms

--- front/src/app/rooms/page.jsx
+++ front/src/app/rooms/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import CardMoreRooms from "@/components/Rooms/CardMoreRooms";
 import NavBarRooms from "@/components/Rooms/NavBarRooms";
@@ -7,28 +7,22 @@ import { CircleSalas } from "@/components/svg/Svgs";
 export default function Rooms() {
   return (
     <>
-      <main className="mainContainer">
-
-        
-        <section className="flex items-center sm:text-start text-center justify-center p-4 gap-8">
-        <NavBarRooms />
-          <div className="flex items-center sm:items-start gap-4">
-            <p className="sm:text-lg text-lg text-primary-50">
-              En esta sección puedes ver las salas que creaste
-            </p>
-
-          </div>
-          <CircleSalas />
-        </section>
-        
-        <section>
-       
-        <CardMoreRooms />
-        </section>
-     
-       
-    
-      </main>
+      <NavBarRooms className="w-full" />
+      <div className="flex flex-col h-screen w-[screen] gap-4 p-4">
+        <main className="mainContainer">
+          <section className="flex items-center sm:text-start text-center justify-center p-4 gap-8 ">
+            <div className="flex items-center sm:items-start gap-4">
+              <p className="sm:text-lg text-lg text-primary-50">
+                En esta sección puedes ver las salas que creaste
+              </p>
+            </div>
+            <CircleSalas />
+          </section>
+          <section>
+            <CardMoreRooms />
+          </section>
+        </main>
+      </div>
     </>
   );
 }
