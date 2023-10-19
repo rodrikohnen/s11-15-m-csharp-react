a6bd6153b44feff0bae637a329118d3214c5d058
sofiacostamagna <sofiacostamagna45@gmail.com>
2023-10-19 11:03:57 -0300

    changes tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,24 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: {
-          DEFAULT: '#545454',
-          50: '#545454',
-          100: '#757575'
+          DEFAULT: "#6785B4",
         },
-        danger: {
-          DEFAULT: '#D9D9D9',
-          50: '#D9D9D9'
-        }
-      }
+        secondary: {
+          DEFAULT: "#563189",
+        },
+        accent: {
+          DEFAULT: "#E94675",
+        },
+        negromate: {
+          DEFAULT: "#020336",
+        },
+        zinc: {
+          DEFAULT: "#F2F2F2",
+        },
+        darkzinc: {
+          DEFAULT: "#D9D9D9",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
