a4890398c0ce33e0b084d8aed10026d86f51b533
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-10-20 16:54:37 -0300

    Change: Adding Styles to Ratings Pages

--- front/src/components/RatingComponentTeacher.jsx
+++ front/src/components/RatingComponentTeacher.jsx
@@ -1,11 +1,26 @@
+"use client";
 import { TeacherRating } from "./TeacherRating";
-import { Mountain } from "./svg/Svgs";
+import Image from "next/image";
+import appRating from "../assets/pictures/appRatingIllustration.svg";
+import ratingComment from "../assets/pictures/ratingCommentIllustration.svg";
+import { useState } from "react";
 
 export const RatingComponentTeacher = () => {
+  const [view, setView] = useState(1);
   return (
     <>
       <div className=" bg-gray-300 rounded-full w-40 h-40 flex items-center justify-center mt-12">
-        <Mountain />
+        {view === 1 ? (
+          <Image
+            src={appRating}
+            alt="Rating Illustration"
+          />
+        ) : (
+          <Image
+            src={ratingComment}
+            alt="Rating Illustration"
+          />
+        )}
       </div>
       <div className=" px-12 mt-8">
         <p className="text-center font-normal text-base">
@@ -13,7 +28,10 @@ export const RatingComponentTeacher = () => {
           <span className="font-medium"> Profesor</span>?
         </p>
       </div>
-      <TeacherRating />
+      <TeacherRating
+        view={view}
+        setView={setView}
+      />
     </>
   );
 };
--- front/src/components/ThanksRatingComponent.jsx
+++ front/src/components/ThanksRatingComponent.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { Mountain } from "./svg/Svgs";
+import Thanks from "../assets/pictures/ThanksIllustration.svg";
+import Image from "next/image";
 
 export const ThanksRatingComponent = () => {
   const router = useRouter();
@@ -12,7 +13,10 @@ export const ThanksRatingComponent = () => {
   return (
     <>
       <div className=" bg-gray-300 rounded-full w-40 h-40 flex items-center justify-center mt-12">
-        <Mountain />
+        <Image
+          src={Thanks}
+          alt="Thank You"
+        />
       </div>
       <div className=" mt-8 w-full">
         <h3 className="text-center font-semibold text-xl mt-12">
@@ -24,7 +28,7 @@ export const ThanksRatingComponent = () => {
       </div>
       <button
         onClick={handleClick}
-        className="pt-1.5 pb-[5px] px-8 rounded-2xl text-white absolute top-[600px] bg-primary">
+        className="pt-1.5 pb-[5px] px-8 rounded-2xl text-white absolute top-[600px] bg-secondary">
         Continuar
       </button>
     </>
