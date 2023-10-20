076528f460e2bc7f5051cab8f5fc091c82040290
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-10-20 10:08:36 -0300

    Change: Tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: {
+<<<<<<< HEAD
 <<<<<<< HEAD
           DEFAULT: "#6785B4",
 <<<<<<< HEAD
@@ -51,7 +52,33 @@ module.exports = {
         }
       }
 >>>>>>> 775c524 (Update tailwind.config.js)
+=======
+          DEFAULT: "#6785B4",
+          50: "#545454",
+          100: "#757575",
+        },
+        secondary: {
+          DEFAULT: "#563189",
+        },
+        danger: {
+          DEFAULT: "#D9D9D9",
+          50: "#D9D9D9",
+        },
+        accent: {
+          DEFAULT: "#E94675",
+        },
+        negromate: {
+          DEFAULT: "#020336",
+        },
+        zinc: {
+          DEFAULT: "#F2F2F2",
+        },
+        darkzinc: {
+          DEFAULT: "#D9D9D9",
+        },
+      },
+>>>>>>> 6e7b7ec (Change: Tailwind config)
     },
   },
   plugins: [],
-}
+};
