790e365866a2154fd687aec1d36d2a06d008b045
Deivison Jimenez <deivi.jimenez@gmail.com>
2023-10-21 11:55:02 -0400

    inicio
    nueva rama corregido

--- front/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="realtive bottom-0 w-full pt-4">
-      <p className="text-center border-black  border-t-[1.5px]">
-        MateSpeak | Copyright © 2023. Todos los derechos reservados
-      </p>
-    </footer>
-  );
-}
