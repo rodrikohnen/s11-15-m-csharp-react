d1b65e4e9636cd18cdef162faee0ebf4b98bfc58
montoyitadevelp <simon1018226487@gmail.com>
2023-10-23 11:22:05 -0500

    fix: Style for the cards

--- front/src/components/CardsHome/ChoiceInfo.jsx
+++ front/src/components/CardsHome/ChoiceInfo.jsx
@@ -1,3 +1,5 @@
+
+
 export default function ChoiceInfo({
   choiceLogo,
   choiceTitle,
@@ -5,9 +7,9 @@ export default function ChoiceInfo({
 }) {
   return (
     <>
-      <div className="flex flex-col text-center items-center py-[2rem] max-w-[15rem] rounded-lg bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
-        <div className="w-28 h-28">{choiceLogo}</div>
-        <h1 className="text-xl font-bold">{choiceTitle}</h1>
+      <div className="flex flex-col text-center items-center py-[2rem] max-w-[15rem] rounded-lg bg-primary shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+        <span className='flex w-32 h-32'>{choiceLogo}</span>
+        <h1 className="text-xl font-bold text-white">{choiceTitle}</h1>
         <p>{choiceDescription}</p>
       </div>
     </>
