64ec49e72ab7bbef896f939e632e0147ebf900d9
montoyitadevelp <simon1018226487@gmail.com>
2023-10-23 11:21:55 -0500

    delete: Quit svgs

--- front/src/components/svg/Svgs.jsx
+++ front/src/components/svg/Svgs.jsx
@@ -1,38 +1,6 @@
 import React from "react";
 
-export const CircleHome = () => {
-  return (
-    <>
-      <div className="rounded-full ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="300"
-          height="300"
-          viewBox="0 0 160 160"
-          fill="none"
-        >
-          <circle cx="80" cy="80" r="80" fill="#D9D9D9" />
-        </svg>
-      </div>
-    </>
-  );
-};
 
-export const CircleChoices = () => {
-  return (
-    <>
-      <div className="rounded-full ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 160 160"
-          fill="none"
-        >
-          <circle cx="80" cy="80" r="80" fill="#D9D9D9" />
-        </svg>
-      </div>
-    </>
-  );
-};
 
 export const CircleTravel = () => {
   return (
