7b52d49ac2e67505c46f87f9e80b4146479edd08
montoyitadevelp <simon1018226487@gmail.com>
2023-10-23 11:22:05 -0500

    fix: Style for the cards

--- front/src/components/SectionTravel/Travelnfo.jsx
+++ front/src/components/SectionTravel/Travelnfo.jsx
@@ -1,17 +1,18 @@
-import { CircleTravel } from "../svg/Svgs";
+import TravelImg from '@/assets/pictures/landing-1.jpg'
+import Image from 'next/image'
 import styled from "@/app/Home.module.css";
 export default function Travelnfo() {
   return (
     <>
       <div className={`${styled.travelInfo}`}>
-        <CircleTravel />
+        <Image className='sm:w-96 sm:h-96 w-72 h-72 rounded-full' src={TravelImg}/>
         <div className={`${styled.travelSubInfo}`}>
           <p>
             Comienza tu viaje lingüístico con Mate Speak, se parte de nuestra
             comunidad global y experimenta una forma emocionante y efectiva de
             aprender un nuevo idioma.
           </p>
-          <div className={`${styled.divButton}`}>Unknown</div>
+          <div className={`${styled.divButton}`}>Registrarse</div>
         </div>
       </div>
     </>
