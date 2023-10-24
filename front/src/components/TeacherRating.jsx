13121081b2d1e25320e33aa8e139ee830f931ba8
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-10-24 17:34:58 -0300

    Change: Rating Styles and Desktop view

--- front/src/components/TeacherRating.jsx
+++ front/src/components/TeacherRating.jsx
@@ -2,25 +2,18 @@
 import Image from "next/image";
 import blueStar from "../assets/pictures/blueStar.svg";
 import whiteStar from "../assets/pictures/whiteStar.svg";
-import { useState } from "react";
 import { RatingTeacherForm } from "./RatingTeacherForm";
-import { useRouter } from "next/navigation";
 
-export const TeacherRating = ({ view, setView }) => {
-  const router = useRouter();
-  const [ratingIsSet, setRatingIsSet] = useState(false);
-  const [isRatingLocked, setIsRatingLocked] = useState(false);
-  const [formValues, setFormValues] = useState({
-    rating: 0,
-    comunicacion: false,
-    interaccion: false,
-    retroalimentacion: false,
-    respuesta: false,
-    otroCheck: false,
-    otroValue: "",
-    comentario: "",
-  });
-
+export const TeacherRating = ({
+  view,
+  setView,
+  setFormValues,
+  formValues,
+  isRatingLocked,
+  setIsRatingLocked,
+  ratingIsSet,
+  setRatingIsSet,
+}) => {
   const handleChange = (index) => {
     if (!isRatingLocked) {
       setFormValues({ ...formValues, rating: index + 1 });
@@ -83,41 +76,23 @@ export const TeacherRating = ({ view, setView }) => {
     setIsRatingLocked(true);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    router.push("/thanksratings");
-    console.log("submit", formValues);
-  };
-
   return (
     <>
-      <form
-        className="flex mt-4 justify-center flex-col items-center"
-        onSubmit={handleSubmit}>
+      <div className="flex mt-4 justify-center flex-col items-center">
         {view === 1 && (
           <aside className="flex gap-8 mt-4 justify-center">
             {renderImages()}
           </aside>
         )}
-        <article>
-          {ratingIsSet && (
-            <RatingTeacherForm
-              formValues={formValues}
-              setFormValues={setFormValues}
-              view={view}
-              setView={setView}
-            />
-          )}
-        </article>
         {ratingIsSet === false && (
           <button
             onClick={handleRatingLock}
             disabled={formValues.rating > 0 ? false : true}
-            className="pt-1.5 pb-[5px] px-8 rounded-2xl text-white absolute top-[600px] bg-secondary disabled:bg-gray-300">
+            className="pt-1.5 pb-[5px] px-8 rounded-2xl text-white absolute top-[600px] bg-secondary disabled:bg-gray-300 lg:static lg:mt-8">
             Continuar
           </button>
         )}
-      </form>
+      </div>
     </>
   );
 };
