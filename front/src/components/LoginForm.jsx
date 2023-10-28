e9895221b1d3645540a38690079060cce5504d5f
jramire5 <jjnicoramirez@gmail.com>
2023-10-28 14:13:58 -0300

    add:Clogin-connection

--- front/src/components/LoginForm.jsx
+++ front/src/components/LoginForm.jsx
@@ -4,12 +4,17 @@ import { object, string, minLength, maxLength, email } from "valibot";
 import { useForm } from "react-hook-form";
 import { useAuthStore } from "@/context/authUser";
 import { useRouter } from "next/navigation";
+<<<<<<< HEAD
 import Link from "next/link";
+=======
+import useLogin from "./../hooks/useLogin";
+>>>>>>> 69f4169 (add:Clogin-connection)
 
+const { handleLogin } = useLogin();
 const LoginSchema = object({
-  correo: string("Debes ingresar caracteres validos.", [
+  correo: string([
     minLength(1, "Ingresa tu email."),
-    email("The email address is badly formatted."),
+    email("El email tiene un formato inválido"),
   ]),
   contraseña: string("Debes ingresar caracteres validos.", [
     minLength(1, "Ingresa tu contraseña"),
@@ -20,26 +25,33 @@ const LoginSchema = object({
 
 export default function LoginForm() {
   const router = useRouter();
-  const login = useAuthStore((state) => state.isLogin);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: valibotResolver(LoginSchema) });
+  } = useForm({
+    resolver: valibotResolver(LoginSchema),
+  });
 
   const onSubmit = (e) => {
+<<<<<<< HEAD
     /*  e.preventDefault(); */
     login();
     router.push("/user");
+=======
+>>>>>>> 69f4169 (add:Clogin-connection)
     console.log("Formulario enviado:", e);
+    handleLogin(e);
+    login();
   };
 
   return (
     <div className="w-full flex justify-center items-center">
       <form
         className="flex flex-col w-full justify-start items-start gap-6 lg:justify-center lg:items-center lg:w-[328px] lg:mt-8"
-        onSubmit={handleSubmit(onSubmit)}>
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <input
           placeholder="Correo electrónico"
           type="email"
@@ -56,6 +68,7 @@ export default function LoginForm() {
         {errors.contraseña && (
           <p className="errormsj">{errors.contraseña?.message}</p>
         )}
+<<<<<<< HEAD
         <aside className="flex flex-col justify-start w-full mb-4">
           <h1 className="text-xl text-black font-extrabold text-left mb-2 lg:text-[26px]">
             Inicia Sesión
@@ -70,6 +83,10 @@ export default function LoginForm() {
         <button
           type="submit"
           className="registerBtn">
+=======
+
+        <button type="submit" className="registerBtn">
+>>>>>>> 69f4169 (add:Clogin-connection)
           Iniciar sesión
         </button>
       </form>
---
+++ front/src/hooks/useLogin.js
@@ -0,0 +1,42 @@
+import { HttpRequest } from "@/helpers/httpRequest";
+import { API_URL, USUARIOS_URL } from "@/libs/routes";
+import { useAuthStore } from "@/context/authUser";
+const useLogin = () => {
+  const redirectLogin = (navigate) => {
+    navigate("/home");
+  };
+
+  const login = useAuthStore((state) => state.isLogin);
+  const handleLogin = async (data) => {
+    console.log("data");
+    const req = await HttpRequest();
+
+    const url = `${API_URL}${USUARIOS_URL}`;
+    const options = {
+      headers: { "content-type": "application/json" },
+      body: {
+        correo: data.correo,
+        contraseña: data.contraseña,
+      },
+    };
+    req.post(url, options).then((res) => {
+      console.log(res, "respuesta");
+      if (res.usuario) {
+        console.log(user);
+        login();
+      }
+    });
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    alert("Te deslogueaste correctamente");
+  };
+
+  return {
+    handleLogin,
+    handleLogout,
+  };
+};
+
+export default useLogin;
