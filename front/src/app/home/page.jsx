f4de9fdbf3497ad021eccc8e6d2916bd7a8c2f1e
jramire5 <jjnicoramirez@gmail.com>
2023-11-01 16:08:48 -0300

    add: styling to search bar in home

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -25,6 +25,7 @@ export default function HomeUser() {
     <main className="mainContainer p-4 sm:p-20">
       {/* Reduzco el espacio en dispositivos pequeños */}
       <div className="flex flex-col mt-4 sm:flex-row sm:mt-12 space-y-4 sm:space-y-0 sm:space-x-8">
+<<<<<<< HEAD
         <CSRHomeGreeting />
         <span className="text-sm sm:flex items-center mt-4 sm-mt-0">
           <input
@@ -33,6 +34,16 @@ export default function HomeUser() {
             className="border border-zinc-400  mb-4 sm:w-72 sm:h-8 rounded-sm"
           />
         </span>
+=======
+        <HomeGreeting />
+      </div>
+      <div className="text-sm sm:flex items-center m-1 ">
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 md:px-4 mt-4"
+        />
+>>>>>>> d281a82 (add: styling to search bar in home)
       </div>
       <h1 className="mt-20 sm:mt-4 text-negromate text-xl font-bold">
         Mis grupos
