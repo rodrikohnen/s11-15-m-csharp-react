04523344aa35e6a34c7ed66f59079172a05d347a
montoyitadevelp <simon1018226487@gmail.com>
2023-11-01 20:18:40 -0500

    fix: Improve dynamic render for client

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -8,15 +8,24 @@ import { LiveCard } from "../rooms/page";
 >>>>>>> b6b0af8 (add: changes for components)
 =======
 import { LiveCard } from "../rooms/page";
+<<<<<<< HEAD
 import { HomeGreeting } from "@/components/HomeGreeting";
 >>>>>>> a6024b4 (Change: Correcciones)
+=======
+
+import dynamic from "next/dynamic";
+
+const CSRHomeGreeting = dynamic(() => import("@/components/HomeGreeting"), {
+  ssr: false
+})
+>>>>>>> a71572a (fix: Improve dynamic render for client)
 
 export default function HomeUser() {
   return (
     <main className="mainContainer p-4 sm:p-20">
       {/* Reduzco el espacio en dispositivos pequeños */}
       <div className="flex flex-col mt-4 sm:flex-row sm:mt-12 space-y-4 sm:space-y-0 sm:space-x-8">
-        <HomeGreeting />
+        <CSRHomeGreeting />
         <span className="text-sm sm:flex items-center mt-4 sm-mt-0">
           <input
             type="text"
--- front/src/app/page.jsx
+++ front/src/app/page.jsx
@@ -1,57 +1,13 @@
-"use client";
-import CardChoices from "@/components/CardsHome/CardChoices";
-import { useRouter } from "next/navigation";
-import TravelInfo from "@/components/SectionTravel/Travelnfo";
-import About from "@/components/CardsHome/CardAbout";
-import Image from "next/image";
-import bgHome from "@/assets/logos/bgHome.webp";
-import useLoginStore from "@/context/loginStore";
+import dynamic from "next/dynamic";
+
+const CSRLanding = dynamic(() => import("@/components/LandingPage"), {
+  ssr: false,
+});
 
 export default function Home() {
-  const router = useRouter();
-  const toRegister = () => {
-    router.push("/signup");
-  };
-  const loginState = useLoginStore((state) => state.isAuth);
-  const loginUser = useLoginStore((state) => state.loginInfo.usuario);
   return (
     <>
-      <main className="space-y-[5rem]">
-        <section className="flex flex-col sm:flex-row sm:text-start text-center items-center justify-center p-[1rem] gap-[2rem]">
-          <div className="flex flex-col items-center sm:items-start gap-[1rem]">
-            <h1 className="sm:text-2xl text-2xl font-bold">
-              ¡Domina un nuevo idioma con Mate Speak!
-            </h1>
-            <p className="sm:text-lg text-md text-primary-50">
-              Comienza hablar el idioma que amas de manera efectiva.
-            </p>
-            {!loginState && (
-              <button
-                className="flex items-center justify-center shadow-xl text-white w-36 h-10 bg-secondary rounded-full"
-                onClick={() => toRegister()}
-              >
-                Registrarse
-              </button>
-            )}
-          </div>
-
-          <Image src={bgHome} />
-        </section>
-        <section className="space-y-5">
-          <h1 className="text-center text-2xl font-bold">
-            ¿Por qué usar Mate Speak?
-          </h1>
-          <div>
-            <CardChoices />
-          </div>
-        </section>
-        <section className="flex justify-center p-[1rem]">
-          <TravelInfo />
-        </section>
-        <section>
-          <About />
-        </section>
-      </main>
+      <CSRLanding />
     </>
   );
 }
